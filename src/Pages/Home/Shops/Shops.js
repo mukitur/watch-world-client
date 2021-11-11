fa95dfcfcf0ba209f79407718a0eac60afe73428
mukitur <mukit.inway@gmail.com>
2021-11-11 13:08:01 +0600

    dynamic route done

--- src/Pages/Home/Shops/Shops.js
+++ src/Pages/Home/Shops/Shops.js
@@ -1,17 +1,56 @@
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 
-const Shops = () => {
+const Shops = ({product}) => {
     const [shops, setShops] = useState({});
-    /* useEffect( ()=>{
-        fetch(`http://localhost:8000/shopping/618be20afd592495193531a4`)
-    }, []) */
+    const {id} = useParams();
+    //const {productName, description, image, price, id} = product;
+    useEffect( ()=>{
+        fetch(`http://localhost:8000/shopping/${id}`)
+        .then(res=>res.json())
+        .then(data=>setShops(data))
+    }, [])
+
     return (
         <>  
             <Navigation></Navigation>
-            <h2>Shop Now Page</h2>
-            <Footer></Footer>
+            <Typography style={{marginTop:'15px'}} variant="h2" component="div" gutterBottom>
+                Shop Now
+            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h4" component="div" gutterBottom>
+                    Purchase {shops.productName}
+                    </Typography>
+                    <Card sx={{ minWidth: 275, border: 0, boxShadow: 0 }}>
+                        <CardMedia
+                            component="img"
+                            style={{ width: '50%', height: '50%x', margin: '0 auto',padding:'5px' }}
+                            image={shops.image}
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                {shops.productName}
+                            </Typography>
+                            <Typography variant="h6" component="div">
+                                Price: BDT {shops.price}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {shops.description}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom component="div">
+                        Please fillup the form below to complete the order.
+                    </Typography>
+                </Grid>
+            </Grid>
         </>
     );
 };
--- src/Pages/Home/SingleProduct/SingleProduct.js
+++ src/Pages/Home/SingleProduct/SingleProduct.js
@@ -1,4 +1,4 @@
-import { Button, Card, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material';
+import { Button, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
