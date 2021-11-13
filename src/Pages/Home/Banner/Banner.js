39827add50fac8a4fdf97d25286b0ec3a741c6f7
mukitur <mukit.inway@gmail.com>
2021-11-13 09:33:05 +0600

    banned add

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,28 +1,118 @@
-import { Grid } from '@mui/material';
-import React from 'react';
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://i.ibb.co/z6GTJF0/1.jpg',
+  },
+  
+  {
+    label: 'Bird',
+    imgPath:
+      'https://i.ibb.co/0nzSgJZ/2.jpg',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://i.ibb.co/G5h8FPh/3.jpg',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://i.ibb.co/tCz5R7h/4.jpg',
+  },
+];
 
 
 const Banner = () => {
-    const mainFeaturedPost = {
-        title: 'Title of a longer featured blog post',
-        description:
-          "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-        image: 'https://source.unsplash.com/random',
-        imageText: 'main image description',
-        linkText: 'Continue reading…',
-      };
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
 
-    return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} sm={5}>
-                Home Page 1st Part
-            </Grid>
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
-            <Grid item xs={12} sm={7}>
-                Home Page 2nd Part       
-            </Grid>
-        </Grid>
-    );
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ maxWidth: '100%', flexGrow: 1 }}>
+      
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 'auto',
+                  display: 'block',
+                  maxWidth: '100%',
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+  );
 };
 
 export default Banner;
